ef93fe75dcad630449f5c09d5ed3812cf3249f03
Shuzhao Feng <90885757+ShuzhaoFeng@users.noreply.github.com>
2023-09-10 17:16:07 -0400

    Add Background Color

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 import re
 
-def eight_hex_to_six_hex(hex_str):
-    regex = re.compile(r'^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})$')
-    if not regex.match(hex_str): return ""
-    transparency_modifier = (int(hex_str[7:9], 16)) / 255
-    red = 255 - ((255 - int(hex_str[1:3], 16)) * transparency_modifier)
-    green = 255 - ((255 - int(hex_str[3:5], 16)) * transparency_modifier)
-    blue = 255 - ((255 - int(hex_str[5:7], 16)) * transparency_modifier)
-    return ('#%02x%02x%02x' % (int(red), int(green), int(blue))).upper()
+eight_digit_hex = re.compile(r'^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})$')
+six_digit_hex = re.compile(r'^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})$')
 
-while True:
-    in_hex = input("Enter an 8-digit hex color code in format \"#RDGRBLTP\", " +
-                   "or hit Enter to exit: ")
-    if in_hex == "": break
-    out_hex = eight_hex_to_six_hex(in_hex)
-    if out_hex == "": print("Invalid input, the input must be in format \"#RDGRBLTP\"")
-    print("The corresponding 6-digit hex color (without transparency) code is", out_hex)
+
+class InvalidBackground:
+    pass
+
+
+def convert(foreground, background):
+
+    background_red = int(background[1:3], 16)
+    background_green = int(background[3:5], 16)
+    background_blue = int(background[5:7], 16)
+
+    foreground_red = int(foreground[1:3], 16)
+    foreground_green = int(foreground[3:5], 16)
+    foreground_blue = int(foreground[5:7], 16)
+
+    foreground_alpha = (int(foreground[7:9], 16)) / 255
+
+    out_red = (1 - foreground_alpha) * background_red + foreground_alpha * foreground_red
+    out_green = (1 - foreground_alpha) * background_green + foreground_alpha * foreground_green
+    out_blue = (1 - foreground_alpha) * background_blue + foreground_alpha * foreground_blue
+
+    return ('#%02x%02x%02x' % (int(out_red), int(out_green), int(out_blue))).upper()
+
+
+def run():
+    try:
+        background_color = input("To set the background color, enter an 8-digit hex color code in format" +
+                                 "\"#RDGRBL\", or hit enter to leave it as default (#FFFFFF): ")
+
+        if background_color == "":
+            background_color = "#FFFFFF"
+        elif not six_digit_hex.match(background_color):
+            print("Invalid input, the input must be in format \"#RDGRBL\"")
+            raise InvalidBackground
+
+        while True:
+            foreground_color = input("Enter an 8-digit hex color code in format \"#RDGRBLTP\", " +
+                                     "or hit Enter to exit: ")
+
+            if foreground_color == "":
+                break
+
+            if not eight_digit_hex.match(foreground_color):
+                print("Invalid input, the input must be in format \"#RDGRBLTP\"")
+
+            out = convert(foreground_color, background_color)
+
+            print("The corresponding 6-digit hex color code is", out)
+    except InvalidBackground:
+        run()
+
+
+run()
